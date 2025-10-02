3220efae8e4036964f2edd970793c1d852016ffe
Mohammad Fardeen <magikal.asmr@gmail.com>
2025-10-02 12:23:56 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { useState, memo, useCallback, useMemo, MouseEvent } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { PageTransition } from "./components/page-transition";
 import { MainLayout } from "./components/main-layout";
 import { useUser } from "@/firebase";
