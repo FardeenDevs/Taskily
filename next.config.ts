1d3447f21eabc8dbdfde2d873ad3548a72d961fa
Mohammad Fardeen <magikal.asmr@gmail.com>
2025-10-03 09:44:54 +0000

    .

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -39,4 +43,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
